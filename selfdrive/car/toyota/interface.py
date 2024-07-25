67f0070bd5f775823f53b8645fb13fa18384edc1
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2024-07-25 16:01:37 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -164,9 +164,9 @@ class CarInterface(CarInterfaceBase):
     tune.deadzoneV = [0.,  0.,  0.001, 0.003, .1, .15]
     ret.stoppingDecelRate = 0.25  # This is okay for TSS-P
     if candidate in TSS2_CAR:
-      ret.vEgoStopping = 0.2
+      ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
-      ret.stoppingDecelRate = 0.05  # reach stopping target smoothly
+      ret.stoppingDecelRate = 0.04  # reach stopping target smoothly
     tune.kpV = [0.88]
     tune.kiBP = [0., 32.]
     tune.kiV = [.4, .2]
