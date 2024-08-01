ce5b9b40db046f8bf0b0ad277f91f9fc333a915e
JSean0930 <122076254+JSean0930@users.noreply.github.com>
2024-08-01 10:08:00 +0800

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -27,7 +27,7 @@ class CarInterface(CarInterfaceBase):
   def get_pid_accel_limits(CP, current_speed, cruise_speed):
     if CP.carFingerprint in TSS2_CAR and Params().get_bool("Marc_Dynamic_Follow"):
       # Allow for higher accel from PID controller at low speeds
-      return CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX_PLUS
+      return CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX
     else:
       return CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX
 
